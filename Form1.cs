39a874d76c80c03f076ce0e87379f90cd67284cc
aifang <aifanghang@163.com>
2014-07-26 23:34:09 +0800

    aifangchange

--- Form1.cs
+++ Form1.cs
@@ -15,5 +15,10 @@ namespace helloGitHub
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "helloGitHub";
+        }
     }
 }
